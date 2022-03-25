eedbb87c6c321ce763eae23aa3a4b63d870f0561
gurkan.aytekin.innovance <gurkan.aytekin@innovance.com.tr>
2022-03-25 18:12:05 +0300

    Work finished.

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 function compute()
 {
-    var principal = parseFloat(document.getElementById("principal").value);
+    const principal = parseFloat(document.getElementById("principal").value);
     if(isAmountNegative(principal)) {
         document.getElementById("principal").focus();
         return alert('You to enter a positive number?');
     }
 
-    var rate = parseFloat(document.getElementById("rate_val").innerText);
-    var years = parseFloat(document.getElementById("years").value);
-    var interest = principal * years * rate /100;
-    var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerText=interest;
+    const rate = parseFloat(document.getElementById("rate_val").innerText);
+    const years = parseFloat(document.getElementById("years").value);
+    const interest = principal * years * rate /100;
+    const year = new Date().getFullYear()+parseInt(years, 10);
+    document.getElementById("result").innerHTML="If you deposit <mark>"+ interest +"</mark>, <br />" +
+    "at an interest rate of <mark>" + rate + "%</mark>.<br>" +
+    "You will receive an amount of <mark>" + principal + "</mark>,<br>" +
+    "in the year <mark>" + year + "</mark><br>";
 }
 
 function isAmountNegative(principal) {
